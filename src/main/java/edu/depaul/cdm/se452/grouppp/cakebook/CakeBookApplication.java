968f7d8c0d089440d007202f8f825247b2ff5ace
Chase Raab <99097952+chaseraab@users.noreply.github.com>
2022-04-05 14:45:40 -0500

    Adding Maven Project

---
+++ src/main/java/edu/depaul/cdm/se452/grouppp/cakebook/CakeBookApplication.java
@@ -0,0 +1,15 @@
+package edu.depaul.cdm.se452.grouppp.cakebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeBookApplication.class, args);
+
+		System.out.println("Welcome to CakeBook!");
+	}
+
+}
---
+++ src/test/java/edu/depaul/cdm/se452/grouppp/cakebook/CakeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.grouppp.cakebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
