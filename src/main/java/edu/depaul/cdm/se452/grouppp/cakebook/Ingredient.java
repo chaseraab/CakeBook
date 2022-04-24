de82b5739faf245c473affbc4a9b4a2ce79fc106
Vee Nguyen <veeyn@bu.edu>
2022-04-24 14:07:55 -0400

    adding ingrediant class

---
+++ src/main/java/edu/depaul/cdm/se452/grouppp/cakebook/Ingredient.java
@@ -0,0 +1,14 @@
+package edu.depaul.cdm.se452.grouppp.cakebook;
+
+import lombok.Data;
+
+@Data
+public class Ingrediant {
+    String name;
+    Integer quantity;
+
+    public Ingrediant(){
+        
+    }
+    
+}
