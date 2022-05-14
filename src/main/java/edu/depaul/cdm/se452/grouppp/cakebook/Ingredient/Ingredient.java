de5900647e906e480a4b697f6e88406b3a3dffff
Chase Raab <99097952+chaseraab@users.noreply.github.com>
2022-05-14 16:39:22 -0500

    Adding JPA for objects as well as functioning database schema

--- src/main/java/edu/depaul/cdm/se452/grouppp/cakebook/Ingredient/Ingredient.java
+++ src/main/java/edu/depaul/cdm/se452/grouppp/cakebook/Ingredient/Ingredient.java
@@ -3,6 +3,8 @@ package edu.depaul.cdm.se452.grouppp.cakebook.Ingredient;
 import lombok.Data;
 import javax.persistence.*;
 
+import edu.depaul.cdm.se452.grouppp.cakebook.Recipe.Recipe;
+
 @Data
 @Entity
 @Table(name = "Ingredients")
@@ -10,6 +12,10 @@ public class Ingredient {
     @Id
     @GeneratedValue
     private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Recipe recipe;
+
     String name;
     Integer quantity;
 
--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,37 +1,22 @@
-DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS Cookbooks;
-DROP TABLE IF EXISTS Recipes;
-DROP TABLE IF EXISTS Ingredients;
-DROP TABLE IF EXISTS Mealplans;
+
 DROP TABLE IF EXISTS Users_Cookbooks;
 DROP TABLE IF EXISTS Users_Recipes;
 DROP TABLE IF EXISTS Users_Mealplans;
 DROP TABLE IF EXISTS Cookbooks_Recipes;
 DROP TABLE IF EXISTS Recipes_Ingredients;
 DROP TABLE IF EXISTS Recipes_Instructions;
-
+DROP TABLE IF EXISTS Mealplans;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Cookbooks;
+DROP TABLE IF EXISTS Ingredients;
+DROP TABLE IF EXISTS Instructions;
+DROP TABLE IF EXISTS Recipes;
+create type measurements as enum('cup','tablespoon','teaspoon');
 CREATE TABLE Users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     pword VARCHAR(50) NOT NULL
 );
-
-CREATE TABLE Users_Cookbooks(
-    userId INT REFERENCES Users(id),
-    cookbookId INT REFERENCES Cookbooks(id)
-
-);
-
-CREATE TABLE Users_Mealplans(
-    userId INT REFERENCES Users(id),
-    mealplanId INT REFERNECES Mealplans(id)
-);
-
-CREATE TABLE Users_Recipes(
-    userId INT REFERENCES Users(id),
-    recipeId INT REFERENCES Recipes(id)
-);
-
 CREATE TABLE Cookbooks(
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -39,44 +24,48 @@ CREATE TABLE Cookbooks(
     createdAt DATE NOT NULL,
     updatedAt DATE
 );
-
-CREATE TABLE Cookbooks_Recipes(
-    cookbookId INT REFERENCES Cookbooks(id),
-    recipeId INT REFERENCES Recipes(id)
-);
-
 CREATE TABLE Mealplans(
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     weekOf DATE
 );
-
-create type measurements as enum('cup','tablespoon','teaspoon');
-
 CREATE TABLE Ingredients(
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL,
     quantity FLOAT NOT NULL,
     measurement measurements 
 );
-
 CREATE TABLE Recipes(
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL,
-    instructions text[],
     cookTime TIME,
     prepTime TIME,
     favorite BOOLEAN,
     isPublic BOOLEAN
 );
-
-CREATE TABLE Recipes_Ingredients(
-    recipeId INT REFERENCES Recipes(id),
-    ingredientId INT REFERENCES Ingredients(id)
-);
-
 CREATE TABLE Instructions(
     id SERIAL PRIMARY KEY,
     recipeId INT REFERENCES Recipes(id),
     instruction VARCHAR(50)
+);
+CREATE TABLE Users_Cookbooks(
+    userId INT REFERENCES Users(id),
+    cookbookId INT REFERENCES Cookbooks(id)
+
+);
+CREATE TABLE Users_Mealplans(
+    userId INT REFERENCES Users(id),
+    mealplanId INT REFERENCES Mealplans(id)
+);
+CREATE TABLE Users_Recipes(
+    userId INT REFERENCES Users(id),
+    recipeId INT REFERENCES Recipes(id)
+);
+CREATE TABLE Cookbooks_Recipes(
+    cookbookId INT REFERENCES Cookbooks(id),
+    recipeId INT REFERENCES Recipes(id)
+);
+CREATE TABLE Recipes_Ingredients(
+    recipeId INT REFERENCES Recipes(id),
+    ingredientId INT REFERENCES Ingredients(id)
 );
