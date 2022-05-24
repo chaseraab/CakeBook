ebbb3f8ad3e81a8277f8cef27eee02b0dc528b13
Chase Raab <99097952+chaseraab@users.noreply.github.com>
2022-05-23 22:02:13 -0500

    recipe is now able to report on which instructions it has

--- src/main/java/edu/depaul/cdm/se452/grouppp/cakebook/Recipe/RecipeService.java
+++ src/main/java/edu/depaul/cdm/se452/grouppp/cakebook/Recipe/RecipeService.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.depaul.cdm.se452.grouppp.cakebook.Cookbook.*;
 import edu.depaul.cdm.se452.grouppp.cakebook.User.*;
+import edu.depaul.cdm.se452.grouppp.cakebook.Instruction.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -85,12 +86,21 @@ public class RecipeService {
         }
     }
 
+    public ResponseEntity<List<Instruction>> getRecipeInstructions(Long id) {
+        if (recipeRepository.findById(id).isPresent()) {
+            List<Instruction> instructions = new ArrayList<Instruction>();
+            instructions = recipeRepository.findById(id).get().getInstructions();
+            return new ResponseEntity<>(instructions, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
     public ResponseEntity<Recipe> updateRecipe(long id, Recipe newRecipe) {
         Optional<Recipe> searchRecipe = recipeRepository.findById(id);
         if (searchRecipe.isPresent()) {
             Recipe _recipe = searchRecipe.get();
             _recipe.setName(newRecipe.getName());
-            //_recipe.setCookbook(newRecipe.getCookbook());
             _recipe.setIngredients(newRecipe.getIngredients());
             _recipe.setInstructions(newRecipe.getInstructions());
             _recipe.setName(newRecipe.getName());
